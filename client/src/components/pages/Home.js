80d56441d6742593c2e49279cb5f0e243c3997ea
Gavin <gavinporlich@gmail.com>
2023-02-27 19:37:06 -0600

    Dashboard started/ErrorPage made

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import NavBar from "../NavBar";
 
-export default function Home() {
+export default function Example() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <div className="min-h-full">
+        <NavBar />
+
+        <header className="bg-white shadow">
+          <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+              Social Poster
+            </h1>
+          </div>
+        </header>
+        <main>
+          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            <Outlet />
+          </div>
+        </main>
+      </div>
+    </>
+  );
 }
